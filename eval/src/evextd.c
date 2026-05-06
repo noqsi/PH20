f79fc7b70c256f70392eca9f0b82f3f81347bb32
John Doty <jpd@noqsi.com>
2026-05-06 13:40:13 -0400

    strip socket stuff from exextd

--- eval/src/evextd.c
+++ eval/src/evextd.c
@@ -8,70 +8,13 @@
 #include <string.h>
 #include <signal.h>
 
-int init_my_port (unsigned port)
-{
-// Create socket
-
-    int             mys = socket (AF_INET, SOCK_STREAM, 0);
-    if (mys < 0) {
-	perror ("init_my_port; socket");
-	exit (1);
-    }
-
-// Bind it to the given port
-
-    struct sockaddr_in sa;
-    memset((char *) &sa, 0, sizeof(struct sockaddr_in));
-    sa.sin_family = PF_INET;
-    /* in_addr has been zeroed into being INADDR_ANY */
-    sa.sin_port = htons(port);
-    if (bind (mys, (struct sockaddr *) &sa, sizeof (sa)) < 0) {
-	perror ("init_my_port; bind");
-	exit (1);
-    }
-    
-// Make it listen
-
-    if( listen (mys, 5) != 0 ) {
-    	perror( "init_my_port; listen");
-	exit( 1 );
-    }
-    
-    printf( "Listening on socket %d\n", mys );
-    
-    return (mys);
-}
-
-FILE *connect_client( int sock ) {
-	
-	struct sockaddr remote;
-	socklen_t remotel;
-	
-	printf( "Try accept on socket %d\n", sock );
-	
-	int fd = accept( sock, &remote, &remotel );
-	if( fd < 0 ) {
-		perror( "accept" );
-		exit( 1 );
-	}
-	FILE *stream = fdopen( fd, "r+" );
-	if( !stream ) {
-		perror( "opening client connection");
-		exit( 1 );
-	}
-	
-	printf( "Opened a stream on fd %d\n", fd );
-	
-	return stream;
-}
-
 void check_id( uint32_t id, uint32_t expected ) {
 	if( id == expected ) return;
 	fprintf( stderr, "Expected packet ID %X, saw %X\n", expected, id );
 	exit( 1 );
 }
 
-void go( struct ad4080 adc, struct parameters p, FILE *client ) {
+void go( struct ad4080 adc, struct parameters p ) {
 // These are a bit bigger than necessary, but it's simple to make them fixed size
 	sample_t sums[ BUFFER_SAMPLES ], diffs[ BUFFER_SAMPLES ];
 	const uint32_t datid = DATA_ID;
@@ -86,12 +29,12 @@ void go( struct ad4080 adc, struct parameters p, FILE *client ) {
 		while ( e = trigger_search( 
 			diffs, BUFFER_SAMPLES - 2 * p.shape + 1, &p )) {
 			
-			written = fwrite( &datid, sizeof datid, 1, client );
+			written = fwrite( &datid, sizeof datid, 1, stdout );
 			if( written != 1 ) return;
-			written = fwrite( e, sizeof *e, 1, client );
+			written = fwrite( e, sizeof *e, 1, stdout );
 			if( written != 1 ) return;
 			written =  fwrite( samples + e->peak, 
-				sizeof( sample_t ), 2 * p.shape, client );
+				sizeof( sample_t ), 2 * p.shape, stdout );
 			if( written != 2 * p.shape ) return;
 		}
 	}
@@ -102,16 +45,15 @@ int main( int argc, char *argv[] ) {
 	int sock = init_my_port ( EXTRACTOR_PORT );
 	struct ad4080 adc = connect_4080();
 	
-	signal(SIGPIPE, SIG_IGN);	// let stdio see the break
+//	signal(SIGPIPE, SIG_IGN);	// let stdio see the break
 	
 // Main loop
 
 	for(;;) {
-		FILE *client = connect_client( sock );
 		
 // Read the start message
 		uint32_t id;
-		int items = fread( &id, sizeof id, 1, client );
+		int items = fread( &id, sizeof id, 1, stdin );
 		if( items != 1 ) {
 			perror( "start");
 			exit( 1 );
@@ -119,14 +61,13 @@ int main( int argc, char *argv[] ) {
 		check_id( id, START_ID );
 		
 		struct parameters p;
-		items = fread( &p, sizeof p, 1, client );
+		items = fread( &p, sizeof p, 1, stdin );
 		if( items != 1 ) {
 			perror( "parameters");
 			exit( 1 );
 		}
 		
-		go( adc, p, client );	// do the work
+		go( adc, p );	// do the work
 		
-		fclose( client );	// done	
 	}	
 }
